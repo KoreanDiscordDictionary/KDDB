7cec34fbf8ac1e0e786ef1a83895751a0bacf99c
peanut <jjun4341@naver.com>
2021-04-04 21:37:43 +0900

    feat: 디스코드 기본 팁(1~5)

--- cogs/Category.py
+++ cogs/Category.py
@@ -1,15 +1,19 @@
 import discord
 from discord.ext import commands
 import asyncio
+from discord import Embed
+
 
 class Category(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name = '카테고리')
+    @commands.command(name='카테고리')
     async def Category(self, ctx):
-        embed = discord.Embed(title = 'KDD - 카테고리', description = 'KDD의 카테고리입니다. 이모지를 클릭하여 원하는 카테고리를 열어보세요.\n\n:pushpin: 기본적인 디스코드 팁\n📂 디스코드 서버 팁\n🖍 글자 강조하기\n📝 채팅 꾸미기\n🖌 글자 색깔 꾸미기\n⚒ 서버 꾸미기', colour = 0x00FFFF, inline=False)
-        
+        embed = discord.Embed(title='KDD - 카테고리',
+                              description='KDD의 카테고리입니다. 이모지를 클릭하여 원하는 카테고리를 열어보세요.\n\n:pushpin: 기본적인 디스코드 팁\n📂 디스코드 서버 팁\n🖍 글자 강조하기\n📝 채팅 꾸미기\n🖌 글자 색깔 꾸미기\n⚒ 서버 꾸미기',
+                              colour=0x00FFFF, inline=False)
+
         m = await ctx.send(embed=embed)
         await m.add_reaction('📌')
         await m.add_reaction('📂')
@@ -20,42 +24,84 @@ class Category(commands.Cog):
 
         def checks(reaction, user):
             return user == ctx.author and str(reaction.emoji) in ['📌', '📂', '🖍', '📝', '🖌', '⚒']
-        
-        def check(m):
-            return m.author == ctx.author and m.channel == ctx.channel
+
 
         try:
             reaction, user = await self.bot.wait_for('reaction_add', timeout=60.0, check=checks)
 
+            r = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣']
+
             if str(reaction.emoji) == '📌':
-                await ctx.send('보고 싶은 항목의 번호를 입력하세요. (1번을 보고 싶다면 1 입력)\n\n1. 디스코드란 무엇인가\n2. 키보드 콤보 - 디스코드 단축키\n3. eon28이 소개한 봇\n4. 디스코드 오류 해결법\n5. 이모지, 이모티콘\n6. 배지 badges\n7. 프로필 꾸미기 rich presence\n8. 디스코드 문의하기 \n9. HypeSquad 배지 얻기')
-                desc = await self.bot.wait_for("message", check=check)
-                desc = desc.content
+                a = discord.Embed(title='번호 선택',
+                                  description='보고 싶은 항목의 번호에 해당하는 이모지를 클릭하세요. (1번을 보고 싶다면 :one: 클릭)\n\n1. 디스코드란 무엇인가\n2. 키보드 콤보 '
+                                              '- 디스코드 단축키\n3. eon28이 소개한 봇\n4. 디스코드 오류 해결법\n5. 이모지, 이모티콘\n6. 배지 badges\n7. 프로필 '
+                                              '꾸미기 rich presence\n8. 디스코드 문의하기 \n9. HypeSquad 배지 얻기', color=0x00FFFF,
+                                  inline=False)
 
-                if desc == 1 or '1':
-                    await ctx.send('디스코드란?\n디스코드는 게이머를 위한 메신저이다.\n\n#기본 정보\n\n개발자 - Discord lnc.\n발매 - 2015년 3월 6일\n\n#특징\n● 뛰어난 성능과 무료 사용을 토대로 급부상하고 있는 메신저 프로그램\n● 한국에서는 주로 온라인 게임을 즐기는 사람들이 많이 이용하는 편\n● 뛰어난 성능과 간편함\n● 단순한 보이스 채팅말고도 텍스트 채팅과 정보 공유, 관리 기능 등을 지원\n● 게임 내에서 자체적으로 보이스톡 시스템을 지원하더라도 Discord를 병용하거나 Discord만을 이용하는 경우도 많은 편.\nhttps://cdn.discordapp.com/attachments/655651598671937586/655654221173227531/hgfhfhf.PNG')
-                
-                if desc == 1 or '2':
-                    await ctx.send('키보드 콤보 (디스코드 단축키) - 디스코드에서 사용할 수 있는 단축키 모음\n\nI.설명\n디스코드 프로그램에서 Ctrl+/를 동시에 누르면 키보드 콤보라는 작은 화면이 뜨게 됩니다.\n키보드 콤보는 디스코드 내에서 사용할 수 있는 단축키입니다.\n게임을 하거나 다른 작업, 디스코드 내에서 작업할 때도 편리하게 사용할 수 있겠죠?\nhttps://cdn.discordapp.com/attachments/649570312723234827/663283154848579585/dsffdsfdsfdsfdsfs.PNG\n\nII. DDR 화살표 소리\nCtrl+/ (키보드 콤보)에서 상하좌우 화살표 키를 누르면 오른쪽 위의 DDR 화살표에 불이 들어오면서 소리가 난다\nhttps://cdn.discordapp.com/attachments/649570312723234827/663283217801019403/dsasddad.PNG\n\nIII. 이스터에그\nCtrl+/ (키보드 콤보)에서 HH -> NK 를 입력하면 순옥살이 나간다.\n\n참고영상\nhttps://www.youtube.com/watch?v=5ZvzS3aAXEA&app=desktop')
+                aa = await ctx.send(embed=a)
+                for i in r:
+                    await aa.add_reaction(f'{i}')
 
+                def checka(reaction, user):
+                    return user == ctx.author and str(reaction.emoji) in ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣',
+                                                                          '7️⃣', '8️⃣', '9️⃣']
+
+                try:
+                    reaction, user = await self.bot.wait_for('reaction_add', timeout=120.0, check=checka)
+
+                    if str(reaction.emoji) == '1️⃣':
+                        ee = Embed(title='디스코드란?',
+                                   description='디스코드란?\n디스코드는 게이머를 위한 메신저이다.\n\n#기본 정보\n\n개발자 - Discord lnc.\n발매 - 2015년 3월 '
+                                               '6일\n\n#특징\n● 뛰어난 성능과 무료 사용을 토대로 급부상하고 있는 메신저 프로그램\n● 한국에서는 주로 온라인 게임을 즐기는 '
+                                               '사람들이 많이 이용하는 편\n● 뛰어난 성능과 간편함\n● 단순한 보이스 채팅말고도 텍스트 채팅과 정보 공유, '
+                                               '관리 기능 등을 지원\n● 게임 내에서 자체적으로 보이스톡 시스템을 지원하더라도 Discord를 병용하거나 Discord만을 이용하는 '
+                                               '경우도 많은 '
+                                               '편', color=0x00FFFF)
+                        ee.set_thumbnail(url='https://cdn.discordapp.com/attachments/655651598671937586'
+                                               '/655654221173227531/hgfhfhf.PNG')
+                        await aa.edit(embed=ee)
+
+                    elif str(reaction.emoji) == '2️⃣':
+                        ee = Embed(title='키보드 콤보 (디스코드 단축키)', description='키보드 콤보 (디스코드 단축키) - 디스코드에서 사용할 수 있는 단축키 모음\n\nI.설명\n디스코드 프로그램에서 Ctrl+/를 동시에 누르면 키보드 콤보라는 작은 화면이 뜨게 됩니다.\n키보드 콤보는 디스코드 내에서 사용할 수 있는 단축키입니다.\n게임을 하거나 다른 작업, 디스코드 내에서 작업할 때도 편리하게 사용할 수 있겠죠?\nII. DDR 화살표 소리\nCtrl+/ (키보드 콤보)에서 상하좌우 화살표 키를 누르면 오른쪽 위의 DDR 화살표에 불이 들어오면서 소리가 난다\n\n\nIII. 이스터에그\nCtrl+/ (키보드 콤보)에서 HH -> NK 를 입력하면 순옥살이 나간다.\n\n참고영상\nhttps://www.youtube.com/watch?v=5ZvzS3aAXEA&app=desktop', color=0x00FF00)
+                        ee.add_field(name="이미지", description='[키보드 콤보 목록](https://cdn.discordapp.com/attachments/649570312723234827/663283154848579585/dsffdsfdsfdsfdsfs.PNG) \n[상하좌우 키](https://cdn.discordapp.com/attachments/649570312723234827/663283217801019403/dsasddad.PNG)')
+                        await aa.edit(embed=ee)
+
+                    elif str(reaction.emoji) == '3️⃣':
+                        ee = Embed(title='Eon28(김만찬)이 소개한 봇', description='[여기](https://blog.naver.com/PostList.nhn?blogId=alscks140&categoryNo=42&parentCategoryNo=42&skinType=&skinId=&from=menu)를 클릭하여 자세히 알아보세요.', color=0x00FFFF)
+                        await aa.edit(embed=ee)
+
+                    elif str(reaction.emoji) == '4️⃣':
+                        ee = Embed(title='디스코드 오류 해결법', description='1. 디스코드 알림음이 들리지 않습니다.\n> (정확하지는 않지만 해결이 되었던 방법) \n> 껐다 키면 해결이 됩니다.\n\n2. 화면 공유가 되지 않습니다.\n> 화면 공유를 하는 사람의 컴퓨터 운영체제가 \n> 윈도우 7이면 소리가 안들립니다.', color=0x00FFFF)
+                        await aa.edit(embed=ee)
+
+                    elif str(reaction.emoji) == '5️⃣':
+                        ee = Embed(title='디스코드 이모지', description='이모지, 이모티콘 - 다들 알고 있는 이모티콘이다.\n> I. 설명\n디스코드에서 사용하는 이모티콘이다.\n이모지라고도 불리고 이모티콘이라고도 불린다.\n\n> II. 사용방법\n채팅 입력칸 우측에 이모지 버튼이 있어 이모지를 전송할 수 있다.\n누르게 되면 사용할 수 있는 (글로벌) 이모지, 서버 이모지를 골라 사용할 수 있다.\n상단에 검색창도 있어 검색을 통해 빠르게 이모지/이모티콘을 사용할 수 있다.\n자신이 많이 사용하는 이모지/이모티콘이 있다면 :(이름): 형태로 이모티콘을 채팅을 통해 사용할 수 있다.\n추가로 이모지/이모티콘을 통해 채팅에 반응을 추가 할 수 있다.', color = 0x00FFFF)
+                        await aa.edit(embed=ee)
+
+                except asyncio.TimeoutError:
+                    await ctx.send('시간이 초과되었어요!')
 
             if str(reaction.emoji) == '📂':
-                await ctx.send('보고 싶은 항목의 번호를 입력하세요. (1번을 보고 싶다면 1 입력)\n\n1. 개발자 모드\n2. 디스코드 파트너 \n3. 공개 서버 기능\n4. 디스코드 웹후크')
+                await ctx.send(
+                    '보고 싶은 항목의 번호를 입력하세요. (1번을 보고 싶다면 1 입력)\n\n1. 개발자 모드\n2. 디스코드 파트너 \n3. 공개 서버 기능\n4. 디스코드 웹후크')
 
             if str(reaction.emoji) == '🖍':
-                await ctx.send('보고 싶은 항목의 번호를 입력하세요. (1번을 보고 싶다면 1 입력)\n\n1. 기울기 \n2. 굵은 글씨\n3. 밑줄\n4. 취소선\n5. 마크다운 효과 제거\n6. 공백 만들기')
-            
+                await ctx.send(
+                    '보고 싶은 항목의 번호를 입력하세요. (1번을 보고 싶다면 1 입력)\n\n1. 기울기 \n2. 굵은 글씨\n3. 밑줄\n4. 취소선\n5. 마크다운 효과 제거\n6. 공백 만들기')
+
             if str(reaction.emoji) == '📝':
-                await ctx.send('보고 싶은 항목의 번호를 입력하세요. (1번을 보고 싶다면 1 입력)\n\n1. single_line_highlight\n2. multi_line_highlight\n3. code_block\n4. spoiler_tags\n5. single_line_block_quotes\n6. multi_line_block_quotes\n7. color embed')
-            
+                await ctx.send(
+                    '보고 싶은 항목의 번호를 입력하세요. (1번을 보고 싶다면 1 입력)\n\n1. single_line_highlight\n2. multi_line_highlight\n3. code_block\n4. spoiler_tags\n5. single_line_block_quotes\n6. multi_line_block_quotes\n7. color embed')
+
             if str(reaction.emoji) == '🖌':
                 await ctx.send('글자 색깔 꾸미기')
-            
+
             if str(reaction.emoji) == '⚒':
                 await ctx.send('서버 꾸미기')
-            
+
         except asyncio.TimeoutError:
             await ctx.send('시간이 초과되었습니다.\n시간 제한은 60초입니다.')
-    
+
+
 def setup(bot):
     bot.add_cog(Category(bot))
--- cogs/Quiz.py
+++ cogs/Quiz.py
@@ -5,11 +5,13 @@ import random
 import string
 
 coll = MongoClient('mongodb://localhost:27017/').KDDB.quiz
+c = MongoClient('mongodb://localhost:27017/').KDDB.user
 
 class Quiz(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.coll = coll
+        self.c = c
 
     @commands.group(name = '퀴즈')
     async def quiz(self, ctx):
@@ -18,7 +20,7 @@ class Quiz(commands.Cog):
             await ctx.send(embed=embed)
     
     @quiz.command(name = '출제')
-    async def Appearance_quiz(self, ctx, *, title=None):
+    async def Questions_quiz(self, ctx, *, title=None):
         def check(m):
             return m.author == ctx.author and m.channel == ctx.channel
 
@@ -32,7 +34,7 @@ class Quiz(commands.Cog):
             for i in range(leng):
                 result += random.choice(string_pool)
             ranom = random.randint(1, 1000)
-            await ctx.send(f"{title} 퀴즈의 설명은 무엇인가요?")
+            await ctx.send(f"{title} 퀴즈의 설명은 무엇인가요?\n자세한 설명을 적어주세요.\n**줄바꿈 금지**")
             desc = await self.bot.wait_for("message", check=check)
             des = desc.content
 
@@ -54,6 +56,53 @@ class Quiz(commands.Cog):
                 "answer": int(answer)
             })
             await ctx.send(f'퀴즈 등록을 완료했습니다.\n등록하신 퀴즈의 아이디는 `{result + ranom}`입니다.')
+    
+    @quiz.command(name = '목록')
+    async def quiz_que(self, ctx):
+        i = 1
+        embed = discord.Embed(title = '퀴즈 목록', description = ' ', color = 0x7289DA, inline=False)
+        for i in self.coll.find({}):
+            idd = i['_id']
+            title = i['title']
+            des = i['des']
+            num = i['num']
+            embed.add_field(name = f'{title}', value = f'퀴즈 아이디 : {idd}\n\n퀴즈 설명 : {des}\n선택지 : {num}')
+        await ctx.send(embed=embed)
+    
+    @quiz.command(name = '풀기')
+    async def solve_quiz(self, ctx, *, quizid: str):
+        if self.coll.find_one({"_id": str(quizid)}):
+            i = self.coll.find_one({"_id": str(quizid)})
+            def check(m):
+                return m.author == ctx.author and m.channel == ctx.channel
+            
+            idd = i['_id']
+            title = i['title']
+            des = i['des']
+            num = i['num']
+            n = ''
+            n += str(num) + ','
+            for i in range(num - 1):
+                n += str(int(num - 1)) + ','
+            n = n[:-1]
+
+            embed = discord.Embed(title = f'{title}', description = f'퀴즈 아이디 : {idd}\n\n퀴즈 설명 : {des}\n선택지 : {n}', color = 0x7289DA, inline=False)
+            await ctx.send(embed=embed)
+            await ctx.send('정답은 몇 번일까요?')
+
+            answer = await self.bot.wait_for("message", check=check)
+            answers = answer.content
+
+            if i['answer'] == answers:
+                await ctx.send('퀴즈를 맞추셨습니다. 축하드립니다.')
+                if self.c.find_one({"_id": str(ctx.author.id)}):
+                    find = {"_id": str(ctx.author.id)}
+                    setdata = {"$inc": {"right": 1}}
+                    self.c.update_one(find, setdata)
+            else:
+                await ctx.send('정답이 아닙니다. :cry:')
+        else:
+            await ctx.send('해당 아이디를 가진 퀴즈가 존재하지 않습니다.')
                 
            
 
